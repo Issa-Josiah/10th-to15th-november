e9e2a4f4ea450d8edd0a0203df1ffc7bd1ca3117
Issa_josiah <g.lectricals@gmail.com>
2025-11-17 13:28:34 +0300

    folder making

---
+++ Monday/monday.py
@@ -0,0 +1,17 @@
+# Python primitives . starting with print statement
+
+print("Hello World");
+print("Habari dunia");
+
+"""""
+using a separator
+the space between the two clauses have what is input in the separator
+"""""
+print("hello world", "my name is Josiah", sep="-->")
+
+# next look at end
+"""""
+you are going to notice that the clause inside the end will be seen at the end of the statement
+"""""
+
+print("hello world", "my name is Josiah", end="-->")
---
+++ wednesday/wednesday.py
@@ -0,0 +1,4 @@
+print("Hello Human");
+name = input("Enter your name to be recognized: ");
+output= f"Your name is {name}. True or false"
+print(output) 
